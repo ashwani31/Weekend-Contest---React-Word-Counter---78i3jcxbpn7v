56e9f1587723148c786268368c81bcf79be5ae6d
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-03 13:54:51 +0530

    test2

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,12 @@ const App = () => {
   const handleChange = (e) => {
     const text = e.target.value;
 
+    //set no. of characters count
     setCharcount(text.length);
-
+    //set no. of words count
     let w_count = 0;
     if (text.length > 0) {
-      w_count = text.trim().split("").length;
+      w_count = text.trim().split(" ").length;
     }
     setWordcount(w_count);
   };
@@ -34,7 +35,7 @@ const App = () => {
         max="32px"
         onChange={fontsizeChangeHandler}
       />
-      <h3>Word limit input</h3>
+      <h3>word limit input</h3>
       <input
         type="number"
         id="char-limit-input"
@@ -45,13 +46,13 @@ const App = () => {
       <div>
         <textarea
           rows="4"
-          style={{ fontSize: +"px" }}
+          style={{ fontSize: fontsize + "px" }}
           onChange={handleChange}
           maxLength={charLimit}
         />
       </div>
-      <div id="word-counter">Total no. of words written{wordCount}</div>
-      <div id="char-counter">Total no. of character used{charCount}</div>
+      <div id="word-counter">Total no. of words written {wordCount}</div>
+      <div id="char-counter">Total no. of characters used {charCount}</div>
     </div>
   );
 };
